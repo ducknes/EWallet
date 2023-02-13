72d913f321d4acd85f5774980121efff11ee14ab
Ilya Antonov <ilya.anton0v@vk.com>
2023-02-13 23:27:48 +0300

    add random wallet adress

--- go.mod
+++ go.mod
@@ -2,9 +2,7 @@ module infotecs-EWallet
 
 go 1.20
 
-require github.com/julienschmidt/httprouter v1.3.0
-
 require (
-	github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f // indirect
-	github.com/mattn/go-sqlite3 v1.14.16 // indirect
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/mattn/go-sqlite3 v1.14.16
 )
--- internal/user/user.go
+++ internal/user/user.go
@@ -1,17 +1,26 @@
 package user
 
-import "time"
+import "math/rand"
 
 type user struct {
-	WalletAddress       string    `json:"wallet_address"`
-	Balance             uint64    `json:"balance"`
-	LastTransactionTime time.Time `json:"last_transaction_time"`
+	WalletAddress       string `json:"wallet_address"`
+	Balance             uint64 `json:"balance"`
+	LastTransactionTime string `json:"last_transaction_time"`
 }
 
 func (u *user) NewUser() *user {
 	return &user{
-		WalletAddress:       "",
-		Balance:             0,
-		LastTransactionTime: time.Now(),
+		WalletAddress:       u.randomWalletAddressGenerator(),
+		Balance:             100,
+		LastTransactionTime: "",
 	}
+}
+
+func (u *user) randomWalletAddressGenerator() string {
+	letters := "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	newAdress := make([]byte, 25)
+	for i := range newAdress {
+		newAdress[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(newAdress)
 }
